65dcca702e735e77e94756ee46feecc9bf809403
takagotch <dyaccb@gmail.com>
2018-02-13 12:49:36 +0900

    secha app build --development

---
+++ cmd.sh
@@ -0,0 +1,11 @@
+npm install
+npm start
+npm start -- --client-environment=production
+curl http://localhost:8080
+
+
+sencha app install ~/sencha-sdks || secha app upgrade ~/sencah-sdks/ext-<version>
+sencha app build --development
+sencha app build --production
+
+
